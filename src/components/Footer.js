03b9129a4c3ce7713617c9d142578527e4226ece
Poorva Lalawat <poorvalalawat@Poorva-Lalawat-I157s-MacBook-Pro.local>
2022-03-10 17:29:27 +0530

    components

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+  
+  return (
+    <footer className="bg-dark text-light"> 
+      <p className='text-center'>
+      Copyright &copy; MyTodoList
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
